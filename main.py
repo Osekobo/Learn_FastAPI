1f971a527db70448a8ca1bec29fefe5e8ee72caa
Oseko <collinsoseko2003@gmail.com>
2026-05-22 21:48:27 +0300

    Retried login and register routes

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+from models import User
 from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordRequestForm
+from schemas import UserGetRegister, UserPostRegister, UserGetLogin, UserPostLogin
 from sqlalchemy.orm import Session
 from sqlalchemy import select
-from schemas import UserGetRegister, UserPostRegister, UserGetLogin, UserPostLogin
-from models import User
-from utils import (get_db, get_password_hash,
-                   verify_password, create_access_token)
+from utils import (get_db, create_access_token,
+                   get_password_hash, verify_password)
 from datetime import timedelta
 ACCESS = 30
 app = FastAPI()
@@ -13,7 +12,7 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"Version": "1.0"}
+    return {"Version": "1"}
 
 
 @app.post("/register", response_model=UserGetRegister, status_code=201)
@@ -28,17 +27,13 @@ def register(user: UserPostRegister, db: Session = Depends(get_db)):
         db.refresh(new_user)
     except Exception:
         db.rollback()
-        raise HTTPException(status_code=500, detail="User registration failed")
+        raise HTTPException(status_code=400, detail="User registration failed")
     access_token = create_access_token(
-        data={"sub": new_user.email}, expires_delta=timedelta(minutes=ACCESS))
-    return {
-        "id": new_user.id,
-        "name": new_user.name,
-        "phone": new_user.phone,
-        "email": new_user.email,
-        "access_token": access_token,
-        "token_type": "bearer"
-    }
+        data={"sub": user.email}, expires_delta=timedelta(minutes=ACCESS))
+    # return {
+    #     "id": user.user_id
+    # }
+    return new_user
 
 
 @app.post("/login", response_model=UserGetLogin)
@@ -49,5 +44,5 @@ def login(data: UserPostLogin, db: Session = Depends(get_db)):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid email or password")
     access_token = create_access_token(
-        data={"sub": user.email}, expires_delta=timedelta(minutes=ACCESS))
+        data={"sub": data.email}, expires_delta=timedelta(minutes=ACCESS))
     return UserGetLogin(access_token=access_token, token_type="bearer")
