c64c96225410f582ecf2a41ab2698c93f893b1c1
Oseko <collinsoseko2003@gmail.com>
2026-05-25 23:42:54 +0300

    Added mainn.py

---
+++ mainn.py
@@ -0,0 +1,43 @@
+from models import User
+from fastapi import FastAPI, Depends, HTTPException, status
+from schemas import UserGetRegister, UserPostLogin, UserGetLogin, UserPostRegister
+from utils import (get_db, verify_password,
+                   get_password_hash, create_access_token)
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from datetime import timedelta
+ACCESS = 30
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return {"Version": "1.0"}
+
+
+@app.post("/register", response_model=UserGetRegister, status_code=201)
+def register(user: UserPostRegister, db: Session = Depends(get_db)):
+    if db.scalar(select(User).where(User.email == user.email)):
+        raise HTTPException(status_code=400, detail="Email already registered")
+    new_user = User(name=User.name, phone=User.phone,
+                    email=User.email, password=get_password_hash(User.password))
+    try:
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+    except Exception:
+        db.rollback()
+        raise HTTPException(status_code=400, detail="User registration failed")
+    return new_user
+
+
+@app.post("/login", response_model=UserGetLogin)
+def login(data: UserPostLogin, db: Session = Depends(get_db)):
+    email = data.user.email.lower().strip()
+    user = db.scalar(select(User).where(User.email == email))
+    if not user or not verify_password(data.password, user.password):
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED,
+                            detail="Invalid email or password")
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=timedelta(minutes=ACCESS))
+    return UserGetLogin(access_token=access_token, token_type="bearer")
